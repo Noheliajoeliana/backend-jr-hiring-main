9c461fd5263b5d741cf8ff479dcd00acd0626d1a
nohel <nohelia.j.r.a@gmail.com>
2021-12-03 14:10:18 -0400

    feat: create findOne method and explain it

--- test3.js
+++ test3.js
@@ -38,8 +38,39 @@ class Server {
 
     }
 
-    findOne(){
+    findOne(collection,id){
 
+        if(id===null){
+            //If I receive an id=null then I return  NotFound Error
+            return new NotFound('No data found with the id equal as "null".')
+        }
+
+        const queryDic = ['users','orgs','commits'];
+        
+        //First I make sure that the arguments are correct
+        if(!queryDic.includes(collection) || !id){
+            return new BadRequest()
+        }
+
+        try{
+            //Then I look into the db th right collection, and using Sift I get the correct query
+            const arr = this.db[collection]
+            const result = arr.filter(sift({id}))
+            const finded = result[0]
+
+            if(!finded){
+                //In case I got an empty query then I return a NotFound error
+                return new NotFound()
+            }
+
+            //If I successfully get the query, I return it as object and not as array
+            return finded
+
+        }catch(err){
+
+            //In case there has been some error, I create a new ServerError
+            return new ServerError(err)
+        }
     }
 
     updteOne(){
