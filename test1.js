5703387bcdb4637089d517417dfef13d5ed150ae
nohel <nohelia.j.r.a@gmail.com>
2021-12-03 10:59:01 -0400

    feat: explain the function

--- test1.js
+++ test1.js
@@ -1,5 +1,9 @@
 
 module.exports = function Test1(server,name) {
+
+  //According to test1.test.js the function Test1 receives 2 arguments, the server and a random name.
+  //In this function, I set the attribute 'name' in the object server.events with a callback, which receives an arguments and returns it.
+  //The correct way to access to that argument, without hardcoding it is accessing to the result of server.events.name being called with tha name used.
   server.on('name', data => data);
   return server.events.name(name)
 }
